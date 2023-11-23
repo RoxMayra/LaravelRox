00fdb23b8b848ca71c4f12fd8b3b580dc076ce26
RoxMayra <mayrapuma2023@gmail.com>
2023-11-23 10:09:19 -0500

    v2 laravel roxana

---
+++ resources/views/pagGaleria.blade.php
@@ -0,0 +1,9 @@
+@extends('pagPlantilla')
+
+@section('titulo')
+    <h1>Pagina Galeria</h1>
+@endsection
+
+@section('seccion')
+    <h3>Este es el codigo de la foto</h3>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+
+Route::get('/', [PagesController::class , 'fnIndex']) -> name('xInicio');
+Route::get('/galeria/{numero?}', [PagesController::class , 'fnGaleria']) -> where('numero', '[0-9]+') -> name('xGaleria');
+Route::get('/lista', [PagesController::class , 'fnLista']) -> name('xLista');
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +23,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/*
 Route::get('/', function () {
     return view('welcome');
+}) -> name('xInicio');
+
+Route::get('/saludo', function () {
+    return "Hola Roxana desde Laravel";
 });
 
+Route::get('/galeria/{numero}', function ($numero) {
+    return "Este es el codigo de la foto: ".$numero;
+}) -> where('numero', '[0-9]+');
+
+Route::view('/galeria','pagGaleria', ['valor' => 15]) -> name('xGaleria');
+
+
+Route::get('/lista', function () {
+    return view('pagLista');
+}) -> name('xLista');
+
+*/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
